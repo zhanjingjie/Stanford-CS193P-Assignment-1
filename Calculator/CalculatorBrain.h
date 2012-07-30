4bc9e9118908e2b948fbbee3478a07671d7a01ad
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2012-07-30 16:58:23 -0700

    initial commit

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,17 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by apple on 5/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+- (void) pushOperand:(double)operand;
+- (double) performOperation:(NSString *)operation;
+- (void) clearOperation;
+
+@end
